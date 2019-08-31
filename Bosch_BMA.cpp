b02b83101a7df970d7855a356343ff33403174a5
sulkith <sulkith@posteo.de>
2019-08-31 23:06:12 +0200

    refactored readReg/writeReg Interface, so it is easy to add SPI to the lib

--- Bosch_BMA.cpp
+++ Bosch_BMA.cpp
@@ -19,6 +19,15 @@ void writeReg(uint8_t reg, uint8_t val)
   twi_write(BMA_ADDRESS,senData,2,NULL);
   twi_wait();
 }
+void writeReg(uint8_t reg, uint8_t *val, uint8_t len)
+{
+  uint8_t senData[TWI_BUFFER_LENGTH];
+  senData[0]= reg;
+  for(uint8_t i = 0;i<len;++i)senData[i+1]=val[i];
+  twi_write(BMA_ADDRESS,senData,len+1,NULL);
+  twi_wait();
+  return;
+}
 //Callback funtion for reading more than one byte
 void saveReadData(uint8_t address, uint8_t *data)
 {
@@ -38,7 +47,25 @@ uint8_t readReg(uint8_t reg, uint8_t len)
   twi_read(BMA_ADDRESS,len,&saveReadData);
   twi_wait();
   return readBuffer[0];
+}
 
+uint8_t readRegs(uint8_t reg, uint8_t len, uint8_t *buffer)
+{
+	uint8_t senData = reg;
+  twi_write(BMA_ADDRESS,&senData,1,NULL); //Send reg address
+  twi_wait();
+  readLen = len;//set the length for the read Data
+  twi_read(BMA_ADDRESS,len,&saveReadData);
+  twi_wait();
+  //so this function is also usable for just one Byte
+  if(buffer != NULL)
+  {
+    for(uint8_t i = 0;i<len;++i)
+    {
+      buffer[i] = readBuffer[i];
+    }
+  }
+  return readBuffer[0];
 }
 void stream_write(uint8_t * data, uint16_t index, uint8_t length)
 {
@@ -49,14 +76,10 @@ void stream_write(uint8_t * data, uint16_t index, uint8_t length)
 	writeReg(BMA4_RESERVED_REG_5B_ADDR, asic_lsb);
   writeReg(BMA4_RESERVED_REG_5C_ADDR, asic_msb);
 
-  uint8_t sendData[16+1];
-  sendData[0] = BMA4_FEATURE_CONFIG_ADDR;
-  for(uint8_t i = 0;i<length;++i)
-  {
-    sendData[i+1] = data[i];
-  }
-  twi_write(BMA_ADDRESS,sendData,length+1,NULL);
+  writeReg(BMA4_FEATURE_CONFIG_ADDR, data, length);
+  return;
 }
+
 uint8_t writeConfigFile()
 {
 	const uint8_t chunkSize = 16;
@@ -164,58 +187,32 @@ void Bosch_BMA::writeInternalConfig(uint8_t *data)
 }
 int16_t Bosch_BMA::get_X_Accel()
 {
-  uint8_t senData = BMA4_X_AXIS;
-  twi_write(BMA_ADDRESS,&senData,1,NULL); //Send reg address
-  twi_wait();
-  readLen = InterruptConfigLenght;//set the length for the read Data
-  twi_read(BMA_ADDRESS,InterruptConfigLenght,&saveReadData);
-  twi_wait();
-  return (int16_t)((readBuffer[1])<<8|readBuffer[0]);
+  uint8_t databuffer[2];
+  readRegs(BMA4_X_AXIS,2,databuffer);
+  return (int16_t)((databuffer[1])<<8|databuffer[0]);
 }
 int16_t Bosch_BMA::get_Y_Accel()
 {
-  uint8_t senData = BMA4_Y_AXIS;
-  twi_write(BMA_ADDRESS,&senData,1,NULL); //Send reg address
-  twi_wait();
-  readLen = InterruptConfigLenght;//set the length for the read Data
-  twi_read(BMA_ADDRESS,InterruptConfigLenght,&saveReadData);
-  twi_wait();
-  return (int16_t)((readBuffer[1])<<8|readBuffer[0]);
+  uint8_t databuffer[2];
+  readRegs(BMA4_Y_AXIS,2,databuffer);
+  return (int16_t)((databuffer[1])<<8|databuffer[0]);
 }
 int16_t Bosch_BMA::get_Z_Accel()
 {
-  uint8_t senData = BMA4_Z_AXIS;
-  twi_write(BMA_ADDRESS,&senData,1,NULL); //Send reg address
-  twi_wait();
-  readLen = InterruptConfigLenght;//set the length for the read Data
-  twi_read(BMA_ADDRESS,InterruptConfigLenght,&saveReadData);
-  twi_wait();
-  return (int16_t)((readBuffer[1])<<8|readBuffer[0]);
+  uint8_t databuffer[2];
+  readRegs(BMA4_Z_AXIS,2,databuffer);
+  return (int16_t)((databuffer[1])<<8|databuffer[0]);
 }
 void Bosch_BMA::startInterruptConfig()
 {
-  uint8_t senData = BMA4_FEATURE_CONFIG_ADDR;
-  twi_write(BMA_ADDRESS,&senData,1,NULL); //Send reg address
-  twi_wait();
-  readLen = InterruptConfigLenght;//set the length for the read Data
-  twi_read(BMA_ADDRESS,InterruptConfigLenght,&saveReadData);
-  twi_wait();
-  for(uint8_t i = 0;i<InterruptConfigLenght;++i)
-  {
-    InterruptConfig[i] = readBuffer[i];
-  }
+  readRegs(BMA4_FEATURE_CONFIG_ADDR,InterruptConfigLenght,InterruptConfig);
+  return;
 }
 
 void Bosch_BMA::writeInterruptConfig()
 {
-	uint8_t sendData[InterruptConfigLenght+1];
-	sendData[0] = BMA4_FEATURE_CONFIG_ADDR;
-	for(uint8_t i = 0;i<InterruptConfigLenght;++i)
-	{
-		sendData[i+1] = InterruptConfig[i];
-	}
-	twi_write(BMA_ADDRESS,sendData,InterruptConfigLenght+1,NULL); //send Data
-	twi_wait();
+  writeReg(BMA4_FEATURE_CONFIG_ADDR, InterruptConfig, InterruptConfigLenght);
+  return;
 }
 
 void Bosch_BMA::setMotionDetection(uint16_t threshhold, uint8_t delay, uint8_t axis, uint8_t nomotion)
