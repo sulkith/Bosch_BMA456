93ea7f86cd19e9f97db9ae14fb0e01faef722464
sulkith <sulkith@posteo.de>
2019-09-06 20:29:27 +0200

    meaningful code for the setCS function, should work on the Watch prototype now?

--- TestCode/main_SPI.cpp
+++ TestCode/main_SPI.cpp
@@ -1,12 +1,19 @@
 #include <stdint.h>
+#include <avr/io.h>
 #include "TestCode.h"
 #include "../spi_adap.h"
 
 void setCS(uint8_t t){
 
+	if(t>0)
+		PORTB |= _BV(PB2);
+	else
+
+		PORTB &= ~_BV(PB2);
 }
 
 int main(){
+	DDRB = _BV(PB2);
 	set_SPI_activate_CS(&setCS);
 	init_BMA();
 }
